231035766ce30863a2d1dab58a9e3e1b3ed4008c
browndw <davidwestbrown@gmail.com>
2025-01-19 09:02:53 -0500

    added data

--- pybiber/__init__.py
+++ pybiber/__init__.py
@@ -1,16 +1,17 @@
+# flake8: noqa
 
+# Imports ----
 from .parse_utils import (
-    get_text_paths, readtext, corpus_from_folder,
-    spacy_parse, get_noun_phrases
+    corpus_from_folder,
+    get_noun_phrases,
+    get_text_paths,
+    readtext,
+    spacy_parse,
 )
 
-from .parse_functions import (
-    biber
-)
+from .parse_functions import biber
 
-from .biber_analyzer import (
-    BiberAnalyzer
-)
+from .biber_analyzer import BiberAnalyzer
 
 __all__ = ['get_text_paths', 'readtext', 'corpus_from_folder',
            'spacy_parse', 'get_noun_phrases', 'biber', 'BiberAnalyzer']
